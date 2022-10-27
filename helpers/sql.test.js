bd714e0cc62f1fdbcdd513fa542595e54d4bb898
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-10-26 22:10:58 -0400

    Part 1 (setup) including creating sql.test.js

---
+++ helpers/sql.test.js
@@ -0,0 +1,7 @@
+// Testing for sql.js and specifically function sqlForPartialUpdate()
+
+const { describe, default: test } = require('node:test');
+
+describe('Using sqlForPartialUpdate', function() {
+	test('foo', function() {});
+});
