171420ee9c726fbd5445aa392053a031e914856b
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-10-28 23:54:30 -0400

    Step one, testing in helpers (sql.test.js)

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,20 +1,18 @@
-const { BadRequestError } = require("../expressError");
+const { BadRequestError } = require('../expressError');
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+// When running this function (found in models), take user data such as {firstName: 'Name', age: 99} for first argument and jsToSql such as { firstName: 'first_name', age: 'age'} for second argument.  Return array with SQL column names and $1, $2, etc. as variables.
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
+	const keys = Object.keys(dataToUpdate);
+	if (keys.length === 0) throw new BadRequestError('No data');
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-  );
+	// {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+	const cols = keys.map((colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`);
 
-  return {
-    setCols: cols.join(", "),
-    values: Object.values(dataToUpdate),
-  };
+	return {
+		setCols: cols.join(', '),
+		values: Object.values(dataToUpdate)
+	};
 }
 
 module.exports = { sqlForPartialUpdate };
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,7 +1,12 @@
 // Testing for sql.js and specifically function sqlForPartialUpdate()
 
-const { describe, default: test } = require('node:test');
+const { sqlForPartialUpdate } = require('./sql');
 
 describe('Using sqlForPartialUpdate', function() {
-	test('foo', function() {});
+	test('whether sqlForPartialUpdate func works', function() {
+		let data = { firstName: 'Name', age: 99 };
+		let jsToSql = { firstName: 'first_name', age: 'age' };
+		let result = sqlForPartialUpdate(data, jsToSql);
+		expect(result).toEqual({ setCols: '"first_name"=$1, "age"=$2', values: [ 'Name', 99 ] });
+	});
 });
